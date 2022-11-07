7648d4e431d6a0dfcf2f905d1e5126822ad36564
nembo93 <francesco.c193@libero.it>
2022-11-07 15:09:21 +0100

    Dichiara  array con oggetti e aggiunge cicli per stampare valori delgli oggetti per ogni oggetto dell'array

--- js/main.js
+++ js/main.js
@@ -1,1 +1,40 @@
 `use strict`;
+
+const team =[
+    {
+        "memberName": `Wayne Barnett`,
+        "role": `Founder and Ceo`,
+        "picture": `wayne-barnett-founder-ceo.jpg`
+    },
+    {
+        "memberName": `Angela Caroll`,
+        "role": `Chief Editor`,
+        "picture": `angela-caroll-chief-editor.jpg`
+    },
+    {
+        "memberName": `Walter Gordon`,
+        "role": `Office manager`,
+        "picture": `walter-gordon-office-manager.jpg`
+    },
+    {
+        "memberName": `Angela Lopez`,
+        "role": `Social media manager`,
+        "picture": `angela-lopez-social-media-manager.jpg`
+    },
+    {
+        "memberName": `Scott Estrada`,
+        "role": `Developer`,
+        "picture": `scott-estrada-developer.jpg`
+    },
+    {
+        "memberName": `Barbara Ramos`,
+        "role": `Graphic Designer`,
+        "picture": `barbara-ramos-graphic-designer.jpg`
+    }
+]
+// console.log(team);
+for(i=0; i<team.length; i++){
+    for (let key in team[i]){
+        console.log(team[i][key]);
+    }
+}
